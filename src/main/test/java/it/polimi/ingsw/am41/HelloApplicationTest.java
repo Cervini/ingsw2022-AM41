555830eab3cf530e02b535ff137983b360c0e7d6
Cervini <simone1.cervini@mail.polimi.it>
2022-03-12 19:02:24 +0100

    Project created

---
+++ src/main/test/java/it/polimi/ingsw/am41/HelloApplicationTest.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.am41;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple HelloApplication
+ */
+public class HelloApplicationTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue(){
+        assertTrue(true);
+    }
+}
