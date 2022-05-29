57c6f7a7f6ac84d170bbfdd00f61140fda5f891d
Davide Fugazza <davo.fuga@gmail.com>
2022-05-29 18:21:16 +0200

    Beginning of GUI implementation

---
+++ src/main/java/it/polimi/ingsw/graphics/Login.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.graphics;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Login extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //Parent root = FXMLLoader.load(getClass().getResource("graphics/fxml/Login.fxml"));
+        primaryStage.setTitle("Eriantys - Login");
+        //primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
